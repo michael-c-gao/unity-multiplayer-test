513b2246ed55837508c6fa07b34da3e72a9f3652
michael-c-gao <michaelgao16@gmail.com>
2021-09-06 20:01:43 -0700

    fixed player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,56 +9,31 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController CC;
     public float movementSpeed;
     public float startSpeed;
-    public float boostSpeed;
-    public float boostMax = 3;
-    public float accumulatedBoostTime = 0f;
-    public float powerupSpeed = 0;
-    public bool boostable = true;
-    public bool subtractSecond = false;
-    public bool powerActive = false;
     private float rotationSensitivity;
     public Transform playerRotate;
     float XRotation = 0f;
     PhotonView PV;
-    //public GameObject[] gunArray;
-    //private Shoot shoot;
-    //swag
 
-    //public Image boostBar;
 
-    public Vector3 movement;
+    //public Vector3 movement;
 
     void Awake()
     {
-        //PV = GetComponent<PhotonView>();
-        PV = GetComponentInParent<PhotonView>();
+        PV = GetComponent<PhotonView>();
     }
 
     public void Start()
     {
 
         movementSpeed = startSpeed;
-        rotationSensitivity = 125;
+        rotationSensitivity = 125.0f;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         if (!PV.IsMine)
         {
-            Destroy(GetComponent<Camera>().gameObject);
+            Destroy(GetComponentInChildren<Camera>().gameObject);
         }
     }
-
-
-    IEnumerator boostRecharge()
-    {
-        subtractSecond = true;
-
-        yield return new WaitForSeconds(2);
-
-        boostable = true;
-        subtractSecond = false;
-        //boostBar.fillAmount = 1;
-    }
-
 
     void Look()
     {
@@ -78,54 +53,20 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 moveVector = (transform.right * horizontal) + (transform.forward * vertical) + (transform.up * threed);
 
-        if (Input.GetKey(KeyCode.LeftShift) && boostable)
-        {
-            movementSpeed = boostSpeed;
-            accumulatedBoostTime += Time.deltaTime;
-            if (accumulatedBoostTime >= 3f)
-            {
-                boostable = false;
-            }
-        }
-        else
-        {
-
-            //defaultParticle.Play();
-            //boostParticle.Stop();
-            movementSpeed = startSpeed + powerupSpeed;
-        }
-
-
-        if (!boostable && !subtractSecond)
-        {
-            StartCoroutine(boostRecharge());
-            accumulatedBoostTime = 0;
-        }
-
-        /*if (PlayerAttack.abilityActivated)
-        {
-            StartCoroutine(SpecialPower());
-        }*/
-
-        movement = moveVector;
+        //movement = moveVector;
         CC.Move(moveVector * movementSpeed * Time.deltaTime);
     }
 
     void Update()
     {
-        //if (!PV.IsMine)
-        //return;
         if (!PV.IsMine)
             return;
-
-        Look();
-        Move();
-        Debug.Log(PhotonNetwork.GetPing());
-        if (Input.GetKey(KeyCode.Z))
+        else if (PV.IsMine)
         {
-            Application.Quit();
+            Look();
+            Move();
         }
-        }
-    //}
+    }
+
 
 }
